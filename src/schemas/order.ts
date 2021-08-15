933f01de8ed308169ed12ff37c3ccf59716b9924
Javier <sanchez.arisan@gmail.com>
2021-08-15 10:45:43 -0600

    order

--- src/schemas/order.ts
+++ src/schemas/order.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import { IOrdersItem } from '../Order/interfaces/order.interface';
+import { IOrdersItem } from '../order/interfaces/order.interface';
 
 @Schema({ collection: 'orders' })
 export class Order extends Document {
